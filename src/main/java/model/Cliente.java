eb866ac7df8ec98cf1d1eab6e43e21ec8bd83718
Pedro Augusto <pedro.azvd01@gmail.com>
2022-10-15 02:35:57 -0300

    Primeiro Commit

---
+++ src/main/java/model/Cliente.java
@@ -0,0 +1,70 @@
+package model;
+
+public class Cliente {
+
+	private int id;
+	private String nome;
+	private String cpf;
+	private String nascimento;
+	private String situacao;
+	
+	public Cliente() {
+	}
+
+	public Cliente(int id, String nome, String cpf, String nascimento, String situacao) {
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.nascimento = nascimento;
+		this.situacao = situacao;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNascimento() {
+		return nascimento;
+	}
+
+	public void setNascimento(String nascimento) {
+		this.nascimento = nascimento;
+	}
+
+	public String getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(String situacao) {
+		this.situacao = situacao;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nome=" + nome + ", cpf=" + cpf + ", nascimento=" + nascimento + ", situacao="
+				+ situacao + "]";
+	}
+	
+	
+	
+}
